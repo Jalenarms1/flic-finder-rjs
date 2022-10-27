a7fbca842c04d5b0bc2ea7d78b125abd287af7c2
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-10-26 20:15:59 -0700

    added modal functionality

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
@@ -8,6 +8,32 @@ export default function Navbar (props) {
     const [showNext, setShowNext] = useState(false);
     const [showAll, setShowAll] = useState(false);
     const [show, setShow] = useState(false);
+    const [randomMovie, setRandomMovie] = useState({});
+    console.log(randomMovie);
+
+    async function getRandomMovie() {
+      let url = 'https://api.watchmode.com/v1/list-titles/?apiKey=CD5UU4BDUoZl8jOFkq3QEQ2iWo6d1MYOrGSDqIQ8&types=movie'
+      let response = await fetch(url, {
+        method: 'GET'
+      })
+      let data = await response.json();
+      console.log(data);
+      let randomTitle = data.titles[Math.floor(Math.random() * data.titles.length)];
+
+      let res = await fetch(`https://www.omdbapi.com/?i=${randomTitle.imdb_id}&apikey=4282aace`, {
+        method: 'GET'
+      })
+      let titleData = await res.json();
+      console.log(titleData);
+
+      setRandomMovie(titleData);
+
+    }
+    useEffect(() => {
+      if(!show){
+        getRandomMovie();
+      }
+    }, [show])
 
 
     function handleShowMore(){
@@ -33,9 +59,11 @@ export default function Navbar (props) {
     }
 
     function handleShow() {
-      setShow(prevState => {
-        return !prevState
-      })
+      setShow(true)
+    }
+
+    function handleClose() {
+      setShow(false)
     }
     
     return (
@@ -47,15 +75,31 @@ export default function Navbar (props) {
           <div className='movie-container pb-3'>
             <Movie handleShowMore={handleShowMore} handleShowAll={handleShowAll} handleShowLess={handleShowLess} searchInp={searchInp} showNext={showNext} showAll={showAll} />
           </div>
-          <Modal show={show}>
+          <Modal className="fade" show={show} onHide={handleClose}>
             <Modal.Header closeButton>
               <Modal.Title>
-                Modal Header
+                Next Watch
               </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-              
+              <div className="w-100 d-flex justify-content-center">
+                <div className="card card-shadow m-1" style={{width: '22rem'}}>
+                    <img src={randomMovie.Poster} className="card-img-top" alt="..." style={{height: '22rem'}}/>
+                    <div className="card-body card-body-pos bg-dark">
+                        <div className="wrap-movie-info text-light">
+                          <h5 className="card-title">{randomMovie.Title}</h5>
+                          <p className="card-text">Directed by: {randomMovie.Director}</p>
+                          <p>{randomMovie.Plot}</p>
+                        </div>
+                    </div>
+                </div>
+
+              </div>
             </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={getRandomMovie}>Redo</Button>
+              <Button variant="secondary" onClick={handleClose}>Close</Button>
+            </Modal.Footer>
           </Modal>
           
           
