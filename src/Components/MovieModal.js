4923c8159f2d76e2f088f9edee34e5a81a8b90b0
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-04 20:40:36 -0700

    pushing restructured comps, working on cutting unnecessary api call for random movie

--- src/Components/MovieModal.js
+++ src/Components/MovieModal.js
@@ -4,35 +4,41 @@ import Modal from 'react-bootstrap/Modal';
 
 
 export default function MovieModal(props) {
-    const [randomMovie, setRandomMovie] = useState({});
+    // const [randomMovie, setRandomMovie] = useState({});
 
-    console.log(randomMovie);
 
-    async function getRandomMovie() {
-        let url = 'https://api.watchmode.com/v1/list-titles/?apiKey=CD5UU4BDUoZl8jOFkq3QEQ2iWo6d1MYOrGSDqIQ8&types=movie'
-        let response = await fetch(url, {
-          method: 'GET'
-        })
-        let data = await response.json();
-        console.log(data);
+    // const loadNewRandom = () => {
+    //   setRandomMovie(props.movieTitles[Math.floor(Math.random() * props.movieTitles.length)])
+
+    // }
+
+    // async function getRandomMovie() {
+    //     let url = 'https://api.watchmode.com/v1/list-titles/?apiKey=CD5UU4BDUoZl8jOFkq3QEQ2iWo6d1MYOrGSDqIQ8&types=movie'
+    //     let response = await fetch(url, {
+    //       method: 'GET'
+    //     })
+    //     let data = await response.json();
+    //     console.log(data);
 
       
-        let randomTitle = data.titles[Math.floor(Math.random() * data.titles.length)];
+    //     let randomTitle = data.titles[Math.floor(Math.random() * data.titles.length)];
   
-        let res = await fetch(`https://www.omdbapi.com/?i=${randomTitle.imdb_id}&apikey=4282aace`, {
-          method: 'GET'
-        })
-        let titleData = await res.json();
-        console.log(titleData);
+    //     let res = await fetch(`https://www.omdbapi.com/?i=${randomTitle.imdb_id}&apikey=4282aace`, {
+    //       method: 'GET'
+    //     })
+    //     let titleData = await res.json();
+    //     console.log(titleData);
   
-        setRandomMovie(titleData);
+    //     setRandomMovie(titleData);
   
-    }
-    useEffect(() => {
-      if(!props.show){
-        getRandomMovie();
-      }
-    }, [props.show])
+    // }
+    // {useEffect(() => {
+    //   if(!props.show){
+    //     // getRandomMovie();
+    //     loadNewRandom();
+    //   }
+
+    // }, [props.show])}
 
     return (
         <Modal className="fade" show={props.show} onHide={props.handleClose}>
@@ -44,23 +50,23 @@ export default function MovieModal(props) {
             <Modal.Body className="bg-dark">
               <div className="w-100 d-flex justify-content-center">
                 <div className="card const-shadow m-1" style={{width: '22rem'}}>
-                    <img src={randomMovie.Poster} className="card-img-top" alt="..." style={{height: '22rem'}}/>
+                    <img src={props.randomMovie.Poster} className="card-img-top" alt="..." style={{height: '22rem'}}/>
                     <div className="card-body card-body-pos bg-dark">
                         <div className="wrap-movie-info text-light">
-                          <h5 className="card-title">{randomMovie.Title}</h5>
-                          <p className="card-text">Directed by: {randomMovie.Director}</p>
-                          <p>{randomMovie.Plot}</p>
+                          <h5 className="card-title">{props.randomMovie.Title}</h5>
+                          <p className="card-text">Directed by: {props.randomMovie.Director}</p>
+                          <p>{props.randomMovie.Plot}</p>
                         </div>
                     </div>
                     <div className="card-footer">
-                      <a className="btn btn-sm btn-outline-dark" href={`https://www.imdb.com/title/${randomMovie.imdbID}`} rel="noreferrer" target="_blank">View on IMDb</a>
+                      <a className="btn btn-sm btn-outline-dark" href={`https://www.imdb.com/title/${props.randomMovie.imdbID}`} rel="noreferrer" target="_blank">View on IMDb</a>
                     </div>
                 </div>
 
               </div>
             </Modal.Body>
             <Modal.Footer className="bg-dark">
-              <Button variant="primary" onClick={getRandomMovie}>Redo</Button>
+              <Button variant="primary" onClick={props.getRandomMovie}>Redo</Button>
               <Button variant="secondary" onClick={props.handleClose}>Close</Button>
             </Modal.Footer>
         </Modal>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,55 +4,55 @@ import Button from 'react-bootstrap/Button';
 import MovieModal from "./MovieModal";
 
 export default function Navbar (props) {
-    const [searchInp, setSearchInp] = useState('');
-    const [showNext, setShowNext] = useState(false);
-    const [showAll, setShowAll] = useState(false);
-    const [show, setShow] = useState(false);
-    const [genre, setGenre] = useState('none');
-    console.log(genre);
+    // const [searchInp, setSearchInp] = useState('');
+    // const [showNext, setShowNext] = useState(false);
+    // const [showAll, setShowAll] = useState(false);
+    // const [show, setShow] = useState(false);
+    // const [genre, setGenre] = useState('none');
+    // console.log(genre);
 
 
-    function handleShowMore(){
-      setShowNext(prevState => {
-        return !prevState
-      })
-    }
+    // function handleShowMore(){
+    //   setShowNext(prevState => {
+    //     return !prevState
+    //   })
+    // }
 
-    function handleShowAll(){
-      setShowAll(prevState => {
-        return !prevState
-      })
-    }
+    // function handleShowAll(){
+    //   setShowAll(prevState => {
+    //     return !prevState
+    //   })
+    // }
 
-    function handleShowLess() {
-      setShowNext(prevState => {
-        return !prevState
-      })
+    // function handleShowLess() {
+    //   setShowNext(prevState => {
+    //     return !prevState
+    //   })
 
-      setShowAll(prevState => {
-        return !prevState
-      })
-    }
+    //   setShowAll(prevState => {
+    //     return !prevState
+    //   })
+    // }
 
-    function handleShow() {
-      setShow(true)
-    }
+    // function handleShow() {
+    //   setShow(true)
+    // }
 
-    function handleClose() {
-      setShow(false)
-    }
+    // function handleClose() {
+    //   setShow(false)
+    // }
 
-    function handleGenreChange(e){
-      setGenre(e.target.value)
-    }
+    // function handleGenreChange(e){
+    //   setGenre(e.target.value)
+    // }
     
     return (
         <div className="wrap-content">
           <div className='navbar navbar-expand-lg nav-bg d-flex justify-content-between'>
             <div className="wrap-left d-flex text-light">
-              <input type="text" placeholder='Search for a title...' onChange={event => {setSearchInp(event.target.value)}}/>
+              <input type="text" placeholder='Search for a title...' onChange={event => {props.setSearchInp(event.target.value)}}/>
               <p className="mx-3 mb-0">Filter by genre: </p>
-              <select onChange={handleGenreChange} className="">
+              <select onChange={props.handleGenreChange} className="">
                 <option value="none">Choose a genre</option>
                 <option name="genre" value="Action">Action</option>
                 <option name="genre" value="Comedy">Comedy</option>
@@ -62,12 +62,12 @@ export default function Navbar (props) {
               </select> 
 
             </div>
-            <Button type="button" onClick={handleShow} className="btn-light rounded p-2">Test your luck</Button>
+            <Button type="button" onClick={props.handleShow} className="btn-light rounded p-2">Test your luck</Button>
           </div>
-          <div className='movie-container pb-3'>
+          {/* <div className='movie-container pb-3'>
             <Movie genre={genre} handleShowMore={handleShowMore} handleShowAll={handleShowAll} handleShowLess={handleShowLess} searchInp={searchInp} showNext={showNext} showAll={showAll} />
-          </div>
-          <MovieModal genre={genre} show={show} handleClose={handleClose} />
+          </div> */}
+          {/* <MovieModal genre={genre} show={show} handleClose={handleClose} /> */}
           
         </div>
     )
