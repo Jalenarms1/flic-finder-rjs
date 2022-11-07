509def7b11387cccaa4b98363d815ae10e32c82b
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-06 21:34:35 -0700

    updated random generator

--- src/Components/MovieModal.js
+++ src/Components/MovieModal.js
@@ -6,10 +6,10 @@ import Modal from 'react-bootstrap/Modal';
 export default function MovieModal(props) {
     const [randomMovie, setRandomMovie] = useState({});
     
-    console.log(props.movies[Math.floor(Math.random() * props.movies.length)]);
+    // console.log(props.movies[Math.floor(Math.random() * props.movies.length)]);
     let randomTitle;
 
-    randomTitle = props.movies[Math.floor(Math.random() * props.movies.length)];
+    // randomTitle = props.movies[Math.floor(Math.random() * props.movies.length)];
 
     
 
@@ -51,16 +51,16 @@ export default function MovieModal(props) {
             <Modal.Body className="bg-dark">
               <div className="w-100 d-flex justify-content-center">
                 <div className="card const-shadow m-1" style={{width: '22rem'}}>
-                    <img src={randomTitle.Poster} className="card-img-top" alt="..." style={{height: '22rem'}}/>
+                    <img src={randomMovie.Poster} className="card-img-top" alt="..." style={{height: '22rem'}}/>
                     <div className="card-body card-body-pos bg-dark">
                         <div className="wrap-movie-info text-light">
-                          <h5 className="card-title">{randomTitle.Title}</h5>
-                          <p className="card-text">Directed by: {randomTitle.Director}</p>
-                          <p>{randomTitle.Plot}</p>
+                          <h5 className="card-title">{randomMovie.Title}</h5>
+                          <p className="card-text">Directed by: {randomMovie.Director}</p>
+                          <p>{randomMovie.Plot}</p>
                         </div>
                     </div>
                     <div className="card-footer">
-                      <a className="btn btn-sm btn-outline-dark" href={`https://www.imdb.com/title/${randomTitle.imdbID}`} rel="noreferrer" target="_blank">View on IMDb</a>
+                      <a className="btn btn-sm btn-outline-dark" href={`https://www.imdb.com/title/${randomMovie.imdbID}`} rel="noreferrer" target="_blank">View on IMDb</a>
                     </div>
                 </div>
 
