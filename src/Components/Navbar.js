6a0e157fdd935e83643c7841f20a6143d83fe89f
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-07 10:07:22 -0700

    fixing

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar (props) {
   return (
       <div className="wrap-content">
         <div className='navbar navbar-expand-lg nav-bg d-flex justify-content-between'>
-          {window.location.pathname !== '!#/saved-movies' ? <div className="wrap-left d-flex text-light">
+          {window.location.pathname !== '#/saved-movies' ? <div className="wrap-left d-flex text-light">
             <input type="text" placeholder='Search for a title...' onChange={event => {props.setSearchInp(event.target.value)}}/>
             <p className="mx-3 mb-0">Filter by genre: </p>
             <select onChange={props.handleGenreChange} className="">
@@ -27,7 +27,7 @@ export default function Navbar (props) {
               <Button type="button" className="btn-light rounded p-2 px-3 mx-2"><Link to="/" style={{textDecoration: 'none', color: 'black'}}>Home</Link></Button>
 
             </div>}
-          {window.location.pathname !== '!#/saved-movies' && <div>
+          {window.location.pathname !== '#/saved-movies' && <div>
             <Button type="button" className="btn-light rounded p-2 mx-2"><Link to="/saved-movies" style={{textDecoration: 'none', color: 'black'}}>Saved Movies</Link></Button>
             <Button type="button" onClick={props.handleShow} className="btn-light rounded p-2">Test your luck</Button>
 
