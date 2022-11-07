e5ac239f613268b205b106e5fce9a4cbec2a6400
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-07 10:53:05 -0700

    tring to figure pathname for hash route

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 
 export default function Navbar (props) {
 
-  console.log(window.location.pathname);
+  console.log(props.location.pathname);
     
     
   return (
