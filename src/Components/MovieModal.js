b42359b75c326e138d0a3817bb0fd21881fabd24
Jalenarms1 <105141749+Jalenarms1@users.noreply.github.com>
2022-11-06 20:25:44 -0700

    added local storage feature

--- src/Components/MovieModal.js
+++ src/Components/MovieModal.js
@@ -1,46 +1,47 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState} from "react";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 
 export default function MovieModal(props) {
-    // const [randomMovie, setRandomMovie] = useState({});
+    const [randomMovie, setRandomMovie] = useState({});
+    
+    console.log(props.movies[Math.floor(Math.random() * props.movies.length)]);
+    let randomTitle;
 
+    randomTitle = props.movies[Math.floor(Math.random() * props.movies.length)];
 
-    // const loadNewRandom = () => {
-    //   setRandomMovie(props.movieTitles[Math.floor(Math.random() * props.movieTitles.length)])
+    
 
-    // }
-
-    // async function getRandomMovie() {
-    //     let url = 'https://api.watchmode.com/v1/list-titles/?apiKey=CD5UU4BDUoZl8jOFkq3QEQ2iWo6d1MYOrGSDqIQ8&types=movie'
-    //     let response = await fetch(url, {
-    //       method: 'GET'
-    //     })
-    //     let data = await response.json();
-    //     console.log(data);
-
+    async function getRandomMovie() {
+        let url = 'https://api.watchmode.com/v1/list-titles/?apiKey=CD5UU4BDUoZl8jOFkq3QEQ2iWo6d1MYOrGSDqIQ8&types=movie'
+        let response = await fetch(url, {
+          method: 'GET'
+        })
+        let data = await response.json();
+        console.log(data);
       
-    //     let randomTitle = data.titles[Math.floor(Math.random() * data.titles.length)];
+        let randomTitle = data.titles[Math.floor(Math.random() * data.titles.length)];
   
-    //     let res = await fetch(`https://www.omdbapi.com/?i=${randomTitle.imdb_id}&apikey=4282aace`, {
-    //       method: 'GET'
-    //     })
-    //     let titleData = await res.json();
-    //     console.log(titleData);
+        let res = await fetch(`https://www.omdbapi.com/?i=${randomTitle.imdb_id}&apikey=4282aace`, {
+          method: 'GET'
+        })
+        let titleData = await res.json();
+        console.log(titleData);
   
-    //     setRandomMovie(titleData);
+        setRandomMovie(titleData);
   
-    // }
-    // {useEffect(() => {
-    //   if(!props.show){
-    //     // getRandomMovie();
-    //     loadNewRandom();
-    //   }
+    }
 
-    // }, [props.show])}
+    useEffect(() => {
+      if(!props.show){
+        getRandomMovie();
+      }
+    
+    }, [props.show])
 
     return (
+      props.show ? 
         <Modal className="fade" show={props.show} onHide={props.handleClose}>
             <Modal.Header className="bg-dark" closeButton closeVariant="white">
               <Modal.Title className="text-light">
@@ -50,26 +51,28 @@ export default function MovieModal(props) {
             <Modal.Body className="bg-dark">
               <div className="w-100 d-flex justify-content-center">
                 <div className="card const-shadow m-1" style={{width: '22rem'}}>
-                    <img src={props.randomMovie.Poster} className="card-img-top" alt="..." style={{height: '22rem'}}/>
+                    <img src={randomTitle.Poster} className="card-img-top" alt="..." style={{height: '22rem'}}/>
                     <div className="card-body card-body-pos bg-dark">
                         <div className="wrap-movie-info text-light">
-                          <h5 className="card-title">{props.randomMovie.Title}</h5>
-                          <p className="card-text">Directed by: {props.randomMovie.Director}</p>
-                          <p>{props.randomMovie.Plot}</p>
+                          <h5 className="card-title">{randomTitle.Title}</h5>
+                          <p className="card-text">Directed by: {randomTitle.Director}</p>
+                          <p>{randomTitle.Plot}</p>
                         </div>
                     </div>
                     <div className="card-footer">
-                      <a className="btn btn-sm btn-outline-dark" href={`https://www.imdb.com/title/${props.randomMovie.imdbID}`} rel="noreferrer" target="_blank">View on IMDb</a>
+                      <a className="btn btn-sm btn-outline-dark" href={`https://www.imdb.com/title/${randomTitle.imdbID}`} rel="noreferrer" target="_blank">View on IMDb</a>
                     </div>
                 </div>
 
               </div>
             </Modal.Body>
             <Modal.Footer className="bg-dark">
-              <Button variant="primary" onClick={props.getRandomMovie}>Redo</Button>
+              <Button variant="primary" onClick={getRandomMovie}>Redo</Button>
               <Button variant="secondary" onClick={props.handleClose}>Close</Button>
             </Modal.Footer>
         </Modal>
+        :
+        <div></div>
     )
 
 
